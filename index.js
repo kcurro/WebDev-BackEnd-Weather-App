cb5bb1d12f2bddaed1f8511bc17f0f9d5e52275d
Katrina Curro <kcurr3@unh.newhaven.edu>
2021-11-29 22:18:33 -0500

    back end framework working

---
+++ index.js
@@ -0,0 +1,49 @@
+//we are now goinh to bring html files here
+
+const http = require('http')
+const fs = require('fs'); //work with the fs
+const path = require ('path'); //we also need to bring in path to create a folder 
+
+const server= http.createServer((req, res) => {
+   
+
+    console.log(req.url);
+    
+    if(req.url ==='/'){
+        //bring index. html
+
+        fs.readFile(path.join(__dirname,'public','index.html'),(err, content)=>{
+
+                if (err) throw err;
+                res.writeHead(200, {'Content-Type': 'text/html'});
+                res.end(content)
+        });       
+    }
+    
+    else if(req.url ==='/api'){
+        // bring about.html
+        fs.readFile(path.join(__dirname,'public','db.json'),(err, content)=>{
+
+            if (err) throw err;
+            res.writeHead(200, {'Content-Type': 'application/json'});
+            res.end(content)
+    });   
+
+    }else {
+        fs.readFile(path.join(__dirname,'public','404.html'),(err, content)=>{
+
+            if (err) throw err;
+            res.writeHead(200, {'Content-Type': 'text/html'});
+            res.end(content)
+    });   
+    }
+
+
+
+
+});
+  
+const PORT =process.env.PORT || 5959
+server.listen(PORT, () => console.log('Server running...'));
+console.log(PORT);
+
